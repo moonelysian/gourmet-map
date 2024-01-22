82e633ad682361380825c84d3802f97d11e7c0ad
moonelysian <yejimun.dev@gmail.com>
2024-01-22 20:53:47 +0900

    add: IntersectionObserver를 사용해 무한 스크롤 구현

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -9,3 +9,10 @@ export interface StoreType {
   storeType?: string | null;
   foodCertifyName?: string | null;
 }
+
+export interface StoreApiResponse {
+  page: number;
+  data: StoreType[];
+  totalCount: number;
+  totalPage: number;
+}
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,20 +1,60 @@
-import Image from "next/image";
+import React, { useRef, useCallback, useEffect } from "react";
+import axios from "axios";
+import { useInfiniteQuery } from "react-query";
+
 import { StoreType } from "@/interface";
 
-import { useQuery } from "react-query";
-
-import axios from "axios";
 import Loading from "@/components/Loading";
+import StoreList from "@/components/StoreList";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import Loader from "@/components/Loader";
 
 export default function StoreListPage() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
+
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios("/api/stores?page=" + pageParam, {
+      params: {
+        limit: 10,
+        page: pageParam,
+      },
+    });
+    return data;
+  };
+
   const {
-    isLoading,
-    isError,
     data: stores,
-  } = useQuery("stores", async () => {
-    const { data } = await axios("/api/stores");
-    return data as StoreType[];
+    isFetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    isError,
+    isLoading,
+  } = useInfiniteQuery("stores", fetchStores, {
+    getNextPageParam: (lastPage: any) =>
+      lastPage.data?.length > 0 ? lastPage.page + 1 : undefined,
   });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => {
+        fetchNext();
+      }, 500);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [fetchNext, isPageEnd, hasNextPage]);
 
   if (isError) {
     return (
@@ -30,41 +70,17 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.map((store, index) => (
-            <li className="flex justify-between gap-x-6 py-5" key={index}>
-              <div className="flex gap-x-4">
-                <Image
-                  src={
-                    store?.category
-                      ? `/images/markers/${store?.category}.png`
-                      : "/images/markers/default.png"
-                  }
-                  width={48}
-                  height={48}
-                  alt="아이콘 이미지"
-                />
-                <div>
-                  <div className="text-sm font-semibold leading-6 text-gray-900">
-                    {store?.name}
-                  </div>
-                  <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                    {store?.storeType}
-                  </div>
-                </div>
-              </div>
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <div className="text-sm font-semibold leading-6 text-gray-900">
-                  {store?.address}
-                </div>
-                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                  {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
-                  {store?.category}
-                </div>
-              </div>
-            </li>
+          stores?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.data.map((store: StoreType, i: number) => (
+                <StoreList key={store.id} store={store} index={i} />
+              ))}
+            </React.Fragment>
           ))
         )}
       </ul>
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
     </div>
   );
 }

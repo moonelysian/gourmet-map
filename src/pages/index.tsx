4ba9c0d3bd76448e45cd278705be05548b1dee30
moonelysian <yejimun.dev@gmail.com>
2024-01-22 14:37:12 +0900

    add: 카카오 지도 불러오는 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,10 @@
-import Link from "next/link";
-import Layout from "./component/Layout";
+import Layout from "./components/Layout";
+import Map from "./components/Map";
 
 export default function Home() {
   return (
-    <>
-      <h1>Map Index Page</h1>
-      <ul>
-        <li>
-          <Link href={"/stores"}>맛집 목록</Link>
-        </li>
-        <li>
-          <Link href={"/stores/new"}>맛집 생성</Link>
-        </li>
-        <li>
-          <Link href={"/stores/1"}>맛집 상세페이지 </Link>
-        </li>
-        <li>
-          <Link href={"/stores/1"}>맛집 상세페이지 수정</Link>
-        </li>
-        <li>
-          <Link href={"/users/login"}>로그인</Link>
-        </li>
-        <li>
-          <Link href={"/users/mypage"}>마이페이지</Link>
-        </li>
-        <li>
-          <Link href={"/users/mypage"}>찜한 맛집</Link>
-        </li>
-      </ul>
-    </>
+    <Layout>
+      <Map />
+    </Layout>
   );
 }

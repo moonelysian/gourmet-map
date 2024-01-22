2b45f54086866bd2485cda6f6d1328e348514352
moonelysian <yejimun.dev@gmail.com>
2024-01-22 16:37:19 +0900

    add: 마커들 컴포넌트로 분리 및 마커 클릭 시 상세보기 제공 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Layout from "./components/Layout";
 import Map from "./components/Map";
+import Markers from "./components/Markers";
+import StoreBox from "./components/StoreBox";
+
+import * as stores from "@/data/store_data.json";
 
 export default function Home() {
+  const [map, setMap] = useState();
+  const [currentStore, setCurrentStore] = useState();
+  const storeDatas = stores["DATA"];
+
   return (
     <Layout>
-      <Map />
+      <Map setMap={setMap} />
+      <Markers
+        map={map}
+        storeDatas={storeDatas}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
     </Layout>
   );
 }

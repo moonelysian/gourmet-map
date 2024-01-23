52c6f486825ef2e8c417d3ed01818d14bc073b55
moonelysian <yejimun.dev@gmail.com>
2024-01-23 18:49:56 +0900

    add: 맛집 추가 기능 추가

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/interface";
 
 import prisma from "@/db";
+import axios from "axios";
 
 interface ResponseType {
   page?: string;
@@ -15,6 +16,25 @@ export default async function handler(
   res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
   const { page = "", limit = "", q, district }: ResponseType = req.query;
+
+  if (req.method === "POST") {
+    const formData = req.body;
+    const headers = {
+      Authorization: `KakaoAK ${process.env.KAKAO_CLIENT_ID}`,
+    };
+
+    const { data } = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURI(
+        formData.address
+      )}`,
+      { headers }
+    );
+
+    const result = await prisma.store.create({
+      data: { ...formData, lat: data.documents[0].y, lng: data.documents[0].x },
+    });
+    return res.status(200).json(result);
+  }
 
   if (page) {
     const skipPage = parseInt(page) - 1;
